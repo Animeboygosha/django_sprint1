96b8481b5625aa4c16293ae8f3ce53a214aa3cba
Стас Басов <stas.basov@whatever.com>
2024-01-24 20:12:07 +0300

    ystal

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -45,28 +45,23 @@ posts: list[dict[str, any]] = [
 ]
 
 
-dpost_id_detail = {}
-for post in posts:
-    post_id_detail[post['id']] = post
+def index(request: HttpRequest) -> HttpResponse:
+    """Рендерит страницу index.html с постами в обратном порядке."""
+    return render(request, 'blog/index.html', {'posts': posts[::-1]})
 
 
-def index(request):
-    template = 'blog/index.html'
-    context = {'post_list': posts}
-    return render(request, template, context)
+post_ids: list[int] = [post['id'] for post in posts]
 
 
-def post_detail(request, post_id):
-    try:
-        context = {'post': post_id_detail[post_id]}
-    except KeyError:
-        raise Http404("Такого поста не существует")
-    template = 'blog/detail.html'
-    return render(request, template, context)
+def post_detail(request: HttpRequest, post_id: int) -> HttpResponse:
+    """Рендерит страницу detail.html с данными конкретного поста."""
+    if post_id not in post_ids:
+        raise Http404('Page not found (404). Пост не найден.')
+    return render(request, 'blog/detail.html', {'post': posts[post_id]})
 
 
-def category_posts(request, category_slug):
-    template = 'blog/category.html'
-    context = {'category': category_slug}
-    return render(request, template, context)
-    
+def category_posts(request: HttpRequest, category_slug: str) -> HttpResponse:
+    """Рендерит страницу category.html с постами определённой категории."""
+    return render(
+        request, 'blog/category.html', {'category_slug': category_slug}
+    )
